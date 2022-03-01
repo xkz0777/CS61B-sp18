0afe73c5e38e138a56fe493f49600744bc5ae1ac
xkz0777 <821376043@qq.com>
2022-03-01 11:48:08 +0800

    add proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class TestArrayDequeGold {
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        String record = "";
+        for (int j = 0; j < 10000; ++j) {
+            double n = StdRandom.uniform();
+            int i = StdRandom.uniform(1000);
+            if (n < 0.25) {
+                sad.addFirst(i);
+                ads.addFirst(i);
+                record += "addFirst(" + i + ")\n";
+            } else if (n < 0.5) {
+                sad.addLast(i);
+                ads.addLast(i);
+                record += "addLast(" + i + ")\n";
+            } else if (n < 0.75) {
+                if (ads.isEmpty() || sad.isEmpty()) {
+                    continue;
+                }
+                Integer actual = sad.removeFirst();
+                Integer expected = ads.removeFirst();
+                record += "removeFirst()\n";
+                assertEquals(record, actual, expected);
+            } else {
+                if (ads.isEmpty() || sad.isEmpty()) {
+                    continue;
+                }
+                Integer actual = sad.removeLast();
+                Integer expected = ads.removeLast();
+                record += "removeLast()\n";
+                assertEquals(record, actual, expected);
+            }
+        }
+    }
 }
